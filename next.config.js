6ab999adb8eceb3828baedb6b965a937ea605a77
liviyaImit <91047177+liviyaImit@users.noreply.github.com>
2021-12-15 17:30:58 +0530

    added loader in nextconfig

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = withPWA({
   i18n,
+  loader: "cloudinary",
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
